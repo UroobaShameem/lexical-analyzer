03a97affec9df012b238df38f14738b955eda937
UroobaShameem <uroobashameem12@gmail.com>
2023-08-24 09:20:14 +0500

    commit

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 from word_break import word_break
-from tokenization import *
+from tokenization import tokenize
 
-
-def input():
+def read_input():
     with open("input.txt", "r") as source_file:
         source_code = source_file.read()
     return source_code
 
-def output():
-    code= input()
-    tokens = word_break(code)
-    with open("words.txt", "a") as output_file:
-        for token_value,Line_no in tokens:
-            output_file.write(f"{token_value}, {Line_no}\n")
-
-def write_output():
-    LA = tokenize(output())
+def write_output(output_tokens):
     with open("output.txt", "w") as file:
-        for token in LA:
+        for token in output_tokens:
             file.write(f"[{token[0]}, {token[1]}, {token[2]}]\n")
 
-
 def main():
-    input()
-    output()
-    write_output()
+    source_code = read_input()
+    tokens = word_break(source_code)
+
+    with open("words.txt", "a") as output_file:
+        for token_value, Line_no in tokens:
+            output_file.write(f"{token_value}, {Line_no}\n")
+
+    LA = tokenize(tokens)
+    write_output(LA)
 
 if __name__ == "__main__":
     main()
